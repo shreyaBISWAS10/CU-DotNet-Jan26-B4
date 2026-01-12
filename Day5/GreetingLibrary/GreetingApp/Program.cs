47f25435998ce8dceb803293deaff4dc0c802a14
shreyaBISWAS10 <shreyabiswas1033@gmail.com>
2026-01-12 12:46:36 +0530

    Day5

---
+++ Day5/GreetingLibrary/GreetingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using GreetingLibrary;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            string getName = GreetingHelper.GetGreeting(name);
+            Console.WriteLine("Hello, "+ getName);
+        }
+    }
+}
