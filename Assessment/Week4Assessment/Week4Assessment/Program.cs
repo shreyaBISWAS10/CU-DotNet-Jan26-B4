384d16929a33374a90f0ee103affb309b493e3a6
shreyaBISWAS10 <shreyabiswas1033@gmail.com>
2026-01-31 12:56:31 +0530

    Week4Assessment

--- Assessment/Week4Assessment/Week4Assessment/Program.cs
+++ Assessment/Week4Assessment/Week4Assessment/Program.cs
@@ -1,47 +1,77 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Week4Assessment
 {
+    class Patient
+    {
+        public string Name { get; set; }
+        public decimal BaseFee { get; set; }
+
+        public Patient(string name, decimal basefee)
+        {
+            Name = name;
+            BaseFee = basefee;
+        }
+
+        public virtual decimal CalculateFinalBill()
+        {
+            return BaseFee;
+        }
+
+        public Patient() { }
+    }
+
     class Inpatient : Patient
     {
-        public int DaysStayed {  get; set; }
-        public decimal DailyRate {  get; set; }
+        public int DaysStayed { get; set; }
+        public decimal DailyRate { get; set; }
 
-        public Inpatient(string name, decimal basefee, int daysstayed,decimal dailyrate) : base(name, basefee)
+        public Inpatient(string name, decimal basefee, int daysstayed, decimal dailyrate)
+            : base(name, basefee)
         {
-            DailyRate = dailyrate;
             DaysStayed = daysstayed;
+            DailyRate = dailyrate;
         }
+
         public override decimal CalculateFinalBill()
         {
             return BaseFee + (DaysStayed * DailyRate);
         }
     }
+
     class OutPatient : Patient
     {
-        public decimal ProcedureFee {  get; set; }
-        public OutPatient(string name, decimal basefee, decimal procedurefee) : base(name, basefee)
+        public decimal ProcedureFee { get; set; }
+
+        public OutPatient(string name, decimal basefee, decimal procedurefee)
+            : base(name, basefee)
         {
-           ProcedureFee = procedurefee;
+            ProcedureFee = procedurefee;
         }
+
         public override decimal CalculateFinalBill()
         {
             return BaseFee + ProcedureFee;
         }
     }
+
     class EmergencyPatient : Patient
     {
         public int SeverityLevel { get; set; }
-        public EmergencyPatient(string name, decimal basefee, int severitylevel) : base(name, basefee)
+
+        public EmergencyPatient(string name, decimal basefee, int severitylevel)
+            : base(name, basefee)
         {
-            SeverityLevel= severitylevel;
+            SeverityLevel = severitylevel;
         }
+
         public override decimal CalculateFinalBill()
         {
             return BaseFee * SeverityLevel;
         }
-
     }
+
     class HospitalBilling
     {
         private List<Patient> patients = new List<Patient>();
@@ -50,17 +80,15 @@ namespace Week4Assessment
         {
             patients.Add(patient);
         }
+
         public void GenerateDailyReport()
         {
             foreach (Patient p in patients)
             {
-               
-                Console.WriteLine(
-                    $"{p.Name}\t - {p.CalculateFinalBill().ToString("C2")}"
-                );
+                Console.WriteLine($"{p.Name}\t - {p.CalculateFinalBill():C2}");
             }
-
         }
+
         public decimal CalculateTotalRevenue()
         {
             decimal sum = 0;
@@ -70,54 +98,38 @@ namespace Week4Assessment
             }
             return sum;
         }
+
         public int GetInpatientCount()
         {
             int cnt = 0;
             foreach (Patient p in patients)
             {
-               if(p is Inpatient){
+                if (p is Inpatient)
+                {
                     cnt++;
                 }
             }
             return cnt;
-
         }
     }
-    internal class Patient
+
+    internal class Program
     {
-        public string Name {  get; set; }
-        public decimal BaseFee { get; set; }
-
-        public Patient(string name, decimal basefee)
-        {
-            Name = name;
-            BaseFee = basefee;
-        }
-        public virtual decimal CalculateFinalBill()
-        {
-            return BaseFee;
-        }
-        public Patient()
-        {
-
-        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             HospitalBilling billing = new HospitalBilling();
             billing.AddPatient(new Inpatient("Suzanne", 2000, 3, 1500));
             billing.AddPatient(new OutPatient("Samaira", 1000, 3500));
             billing.AddPatient(new EmergencyPatient("Kabeer", 500, 5));
+
             Console.WriteLine("-----------------------------");
             Console.WriteLine("Daily Report:");
             billing.GenerateDailyReport();
             Console.WriteLine("-----------------------------");
-            Console.WriteLine($"Total Revenue is : {billing.CalculateTotalRevenue()}");
+            Console.WriteLine($"Total Revenue is : {billing.CalculateTotalRevenue():C2}");
             Console.WriteLine($"Total Inpatient Count is : {billing.GetInpatientCount()}");
-
-
-                
-
         }
     }
 }
