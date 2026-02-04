6676312e43a237cfc6d80076c3eaab406234596e
shreyaBISWAS10 <shreyabiswas1033@gmail.com>
2026-02-04 15:26:08 +0530

    Completed

---
+++ Week5/Day25/Day25/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Day25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pin = "";
+            Console.Write("Enter 4-digit PIN: ");
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+               
+                if (char.IsDigit(key.KeyChar) && pin.Length < 4)
+                {
+                    pin += key.KeyChar;
+                    Console.Write("*");
+                }
+                
+                else if (key.Key == ConsoleKey.Backspace && pin.Length > 0)
+                {
+                    pin = pin.Substring(0, pin.Length - 1);
+
+                    Console.Write("\b \b");
+                }
+
+                if (pin.Length == 4)
+                    break;
+            }
+            Console.WriteLine("\nPIN capturede successfully");
+            Console.Write("\nEnter System Message: ");
+            string message = Console.ReadLine();
+
+            Console.WriteLine("\nSystem Message Received:");
+            Console.WriteLine(message);
+
+        }
+    }
+}
