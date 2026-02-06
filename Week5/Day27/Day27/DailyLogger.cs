8bbefeaafe59d3bbf1d07368d53103c9061049a6
shreyaBISWAS10 <shreyabiswas1033@gmail.com>
2026-02-06 14:34:02 +0530

    Done

---
+++ Week5/Day27/Day27/DailyLogger.cs
@@ -0,0 +1,22 @@
+﻿namespace Day27
+{
+    internal class DailyLogger
+    {
+        static void Main(string[] args)
+        {
+            string directory = @"..\..\..\";
+            string file = "journal.txt";
+            string filePath = Path.Combine(directory, file);
+            Console.WriteLine("Enter your Daily Reflection:");
+            string reflection = Console.ReadLine();
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine($"[{DateTime.Now}]");
+                sw.WriteLine(reflection);
+                sw.WriteLine();
+
+            }
+            Console.WriteLine("Your reflection was saved successfully");
+        }
+    }
+}
