0f8cefc1cec99b547dd7be6896ed580e59e2a318
shreyaBISWAS10 <shreyabiswas1033@gmail.com>
2026-03-31 11:47:31 +0530

    Done

---
+++ Assessment/Week1Assessment/GreetingLibrary/GreetingLibrary/GreetingHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                return "Guest!";
+
+            }
+            else
+            {
+                return name + "!";
+            }
+        }
+    }
+}
