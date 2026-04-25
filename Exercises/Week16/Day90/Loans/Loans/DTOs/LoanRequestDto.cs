c028a2716702863bebd2a4ae25983ef43b5dbec4
shreyaBISWAS10 <shreyabiswas1033@gmail.com>
2026-04-25 09:54:43 +0530

    Done

---
+++ Exercises/Week16/Day90/Loans/Loans/DTOs/LoanRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Loans.DTOs
+{
+    public class LoanRequestDto
+    {
+        public string ApplicantName { get; set; }
+        public decimal LoanAmount { get; set; }
+        public int TenureMonths { get; set; }
+        public decimal InterestRate { get; set; }
+    }
+
+}
---
+++ Exercises/Week16/Day90/Loans/Loans/Models/LoanApplication.cs
@@ -0,0 +1,18 @@
+﻿namespace Loans.Models;
+public class LoanApplication
+{
+    public int Id { get; set; }
+    public string ApplicantName { get; set; }
+    public int UserId { get; set; }
+
+    public decimal LoanAmount { get; set; }
+    public int TenureMonths { get; set; }
+    public decimal InterestRate { get; set; }
+
+    public decimal MonthlyPremium { get; set; }
+    public decimal TotalPayableAmount { get; set; }
+
+    public string Status { get; set; } // Pending, Approved
+
+    public DateTime CreatedDate { get; set; }
+}
